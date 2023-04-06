b9aec5153045da064679bc5e761c9600d156543d
linards <linardssils1@gmail.com>
2023-04-06 12:31:03 +0300

    Adding an event area

---
+++ Player/Movement.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class Movement : KinematicBody2D
+{
+    const float speed = 250;
+    public void move()
+    {
+        Vector2 inputDirection = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        var Velocity = inputDirection * speed;
+
+        MoveAndSlide(Velocity);
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        move();
+    }
+}
