4aa58bde4e5cab061f20bee580148e0a9f480044
linards <linardssils1@gmail.com>
2023-04-24 21:52:54 +0300

    About screen

--- scenes/MainMenu and AboutGame/MainMenu.cs
+++ scenes/MainMenu and AboutGame/MainMenu.cs
@@ -4,19 +4,19 @@ using System;
 public class MainMenu : Control
 {
     public void _on_Start_pressed()
-    { 
-
+    {
+        GetTree().ChangeScene("res://scenes/MainScene.tscn");
     }
 
     public void _on_About_game_pressed()
-    { 
-    
+    {
+        GetTree().ChangeScene("res://scenes/game/AboutGame.tscn");
     }
 
 
     public void _on_Quit_pressed()
     { 
-    
+    GetTree().Quit();
     }
 
 
