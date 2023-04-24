b2b380fd9424bc4266dac650416a13d948e0f1ea
linards <linardssils1@gmail.com>
2023-04-24 22:58:40 +0300

    End screen

---
+++ scenes/menus/EndGame/Label.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+using System;
+
+public class Label : Godot.Label
+{
+
+    public override void _Ready()
+    {
+        Text = "Paldies par spēli, tavu punktu sakits: " + Score.GetScore();
+    }
+
+
+
+}
