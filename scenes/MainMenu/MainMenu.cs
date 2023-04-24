2ed6f2cb8b6ae7e691e84862687df85b403c8b7d
linards <linardssils1@gmail.com>
2023-04-24 20:47:48 +0300

    Title screen

---
+++ scenes/MainMenu/MainMenu.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class MainMenu : Control
+{
+    public void _on_Start_pressed()
+    { 
+
+    }
+
+    public void _on_About_game_pressed()
+    { 
+    
+    }
+
+
+    public void _on_Quit_pressed()
+    { 
+    
+    }
+
+
+
+}
