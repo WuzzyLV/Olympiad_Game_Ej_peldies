7e2bbc6bcec2db9c4172aa7816c015d5fd79d4d9
Reinis <reinisemilsa@gmail.com>
2023-04-26 02:50:15 +0300

    Sign

--- scenes/menus/MapSceneRoot.cs
+++ scenes/menus/MapSceneRoot.cs
@@ -8,7 +8,8 @@ public class MapSceneRoot : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GetTree().ChangeScene(EndPath);
+        if(WorldCompletion.AllWorldsDone())
+            GetTree().ChangeScene(EndPath);
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
