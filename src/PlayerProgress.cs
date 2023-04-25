0851a6d794fb258a94aa00cc62cac45e34e6742d
Reinis <reinisemilsa@gmail.com>
2023-04-25 19:35:53 +0300

    Possibilty to track which minigames done

--- src/PlayerProgress.cs
+++ src/PlayerProgress.cs
@@ -1,8 +1,20 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public class PlayerProgress : Node
 {
+
+    public enum Tasks
+    {
+        BoatGameBeberlini,
+        FishingBeberlini,
+        TrashPludmale,
+        TestBeberlini,
+        TestPludmale,
+    }
+    //Map for Tasks and bools
+    private static Dictionary<Tasks, bool> _tasksCompleted = new Dictionary<Tasks, bool>();
 
     public static String LastScene = "";
     
@@ -11,9 +23,17 @@ public class PlayerProgress : Node
         
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    
+    public static bool IsTaskCompleted(Tasks task)
+    {
+        return _tasksCompleted[task];
+    }
+
+    public static void TaskFinished(Tasks task)
+    {
+        if (_tasksCompleted.ContainsKey(task))
+            return;
+        
+        _tasksCompleted.Add(task, true);
+    }
 }
