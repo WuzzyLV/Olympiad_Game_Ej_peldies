8e78c2b849faca8ae980d7f429589765acc5362d
Reinis <reinisemilsa@gmail.com>
2023-04-25 23:38:53 +0300

    adding boat middle menu

--- scenes/game/wakeBoarding/CollisionChecker.cs
+++ scenes/game/wakeBoarding/CollisionChecker.cs
@@ -3,16 +3,27 @@ using System;
 
 public class CollisionChecker : Area2D
 {
+
+    private MiddleMenu _middleMenu;
+    public override void _Ready()
+    {
+        _middleMenu = GetNode<MiddleMenu>("/root/Node2D/MiddleMenu");
+    }
+
     public void _on_Area2D_body_entered(Node body)
     { 
         if(body.Name != "Boat" && body.Name != "WakeBoard" && body.Name != "Border_Right" && body.Name != "Border_Left" &&  body.Name != "Border_Up" && body.Name != "Border_Down")
         {
             ObjectSpawner s = GetNode<ObjectSpawner>("/root/Node2D/Node/Timer");
             //TODO improve score system
-            Score.AddScore((int)s.time);
+            int score = (int) s.time;
+            Score.AddScore(score);
             
             PlayerProgress.TaskFinished(PlayerProgress.Tasks.BoatGameBeberlini);
-            GetTree().ChangeScene(PlayerProgress.LastScene);
+            
+            _middleMenu.setScore(score);
+            _middleMenu.show();
+            
         }
     }
 }
