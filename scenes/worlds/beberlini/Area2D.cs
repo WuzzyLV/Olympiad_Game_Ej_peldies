fba74b70bec2c991da3c48c63c229d376d1d68c5
saannija <117991126+saannija@users.noreply.github.com>
2023-04-25 23:24:15 +0300

    scene addons

--- scenes/worlds/beberlini/Area2D.cs
+++ scenes/worlds/beberlini/Area2D.cs
@@ -3,25 +3,25 @@ using System;
 
 public class Area2D : Godot.Area2D
 {
-    bool colliding = false;
-    public void _on_Wakeboard_body_entered(Node body)
-    {
-        if(body.Name == "player")
-            colliding = true;
-    }
-    public void _on_Wakeboard_body_exited(Node body)
-    {
-        if(body.Name == "player")
-            colliding = false;
-    }
+	bool colliding = false;
+	public void _on_Wakeboard_body_entered(Node body)
+	{
+		if(body.Name == "player")
+			colliding = true;
+	}
+	public void _on_Wakeboard_body_exited(Node body)
+	{
+		if(body.Name == "player")
+			colliding = false;
+	}
 
-    public override void _PhysicsProcess(float delta)
-    {
-        if (!colliding)
-            return;
-        if (!Input.IsActionJustPressed("interact"))
-            return;
-        
-        GetTree().ChangeScene("res://scenes/game/WakeBoardingScene.tscn");
-    }
+	public override void _PhysicsProcess(float delta)
+	{
+		if (!colliding)
+			return;
+		if (!Input.IsActionJustPressed("interact"))
+			return;
+		
+		GetTree().ChangeScene("res://scenes/game/WakeBoardingScene.tscn");
+	}
 }
